4c54f773408c942c93346c6685b3390036b39127
Joshua Byrd <phocks@gmail.com>
2021-02-19 08:08:51 +1000

    OK OK

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
+use mongodb::bson::{self, doc, Bson};
+use std::env;
+use std::error::Error;
+use tokio;
+use warp::Filter;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Load the MongoDB connection string from an environment variable:
+    let client_uri = "mongodb://localhost:27017";
+    //   env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
+    // A Client is needed to connect to MongoDB:
+    let client = mongodb::Client::with_uri_str(client_uri.as_ref()).await?;
+    // Print the databases in our MongoDB cluster:
+    println!("Databases:");
+
+    let databases = client.list_database_names(None, None).await?;
+
+    for name in databases {
+        println!("- {}", name);
+    }
+
+    let new_doc = doc! {
+       "title": "Parasite",
+       "year": 2020,
+       "plot": "A poor family, the Kims, con their way into becoming the servants of a rich family, the Parks. But their easy life gets complicated when their deception is threatened with exposure.",
+       "released": Utc.ymd(2020, 2, 7).and_hms(0, 0, 0),
+    };
+
+    println!("{}", new_doc);
+
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+
+    Ok(())
+}
